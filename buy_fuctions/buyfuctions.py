d1d4d55f2c1c2c6371a46a55f77d0ff240768208
atork <71847899+atork@users.noreply.github.com>
2024-02-10 18:54:23 +0100

    modified

--- buy_fuctions/buyfuctions.py
+++ buy_fuctions/buyfuctions.py
@@ -1,5 +1,5 @@
+import Mathematik.math as mt
 #Prices
-score = 19
 tasch_cost=1
 raspb_cost=2
 arduino_cos=3
@@ -8,6 +8,15 @@ gpu_cos=5
 saugroboter_cos=6
 mac_cos=7
 roboterarm_cos=8
+price_dict=[
+    "tasch_cost":{
+        "price":3
+    },
+    "rasp_cost":{
+        "price":2
+    }
+
+]
 
 #General
 def costincrement(cost):
@@ -18,8 +27,8 @@ def costred(cost,costmod):
 
 #Taschenrechner
 def tasch_buy():
-    if(score>=tasch_cost):
-        score-tasch_cost
+    if(mt.get_score>=tasch_cost):
+        mt.get_score-tasch_cost
         tasch_cost=costincrement(tasch_cost)
         return True
     else:
@@ -27,56 +36,56 @@ def tasch_buy():
 
 #RaspberryPI
 def raspberry_buy():
-    if(score>=raspb_cost):
-        score-raspb_cost
+    if(mt.get_score>=raspb_cost):
+        mt.get_score-raspb_cost
         raspb_cost=costincrement(raspb_cost)
         return True
     else:
         return False
 #Arduino    
 def arduino_buy():
-    if(score>=arduino_cos):
-        score-arduino_cos
+    if(mt.get_score>=arduino_cos):
+        mt.get_score-arduino_cos
         arduino_cos=costincrement(arduino_cos)
         return True
     else:
         return False
 #CPU
 def cpu_buy():
-    if(score>=cpu_cos):
-        score-cpu_cos
+    if(mt.get_score>=cpu_cos):
+        mt.get_score-cpu_cos
         cpu_cos=costincrement(cpu_cos)
         return True
     else:
         return False    
 #GPU
 def gpu_buy():
-    if(score>=gpu_cos):
-        score-gpu_cos
+    if(mt.get_score>=gpu_cos):
+        mt.get_score-gpu_cos
         gpu_cos=costincrement(gpu_cos)
         return True
     else:
         return False 
 #Saugroboter
 def saugroboter_buy():
-    if(score>=saugroboter_cos):
-        score-saugroboter_cos
+    if(mt.get_score>=saugroboter_cos):
+        mt.get_score-saugroboter_cos
         saugroboter_cos=costincrement(saugroboter_cos)
         return True
     else:
         return False 
 #MAC
 def mac_buy():
-    if(score>=mac_cos):
-        score-mac_cos
+    if(mt.get_score>=mac_cos):
+        mt.get_score-mac_cos
         mac_cos=costincrement(mac_cos)
         return True
     else:
         return False 
 #Roboterarm
 def roboteramrm_buy():
-    if(score>=roboterarm_cos):
-        score-roboterarm_cos
+    if(mt.get_score>=roboterarm_cos):
+        mt.get_score-roboterarm_cos
         roboterarm_cos=costincrement(roboterarm_cos)
         return True
     else:
---
+++ main.py
@@ -0,0 +1,2 @@
+import Mathematik.math as mt
+import buy_fuctions.buyfuctions as bfunct
