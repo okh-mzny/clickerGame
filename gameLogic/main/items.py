04d6df780cc527e5e79be83caa16821917817a05
okh-mzny <60819623+okh-mzny@users.noreply.github.com>
2024-02-12 11:25:00 +0100

    add item Definition

---
+++ gameLogic/main/items.py
@@ -0,0 +1,42 @@
+# Main item definition
+items = (
+    {"id": "tasch",
+     "nameLabel": "Taschenrechner",
+     "cost": 1,
+     "power": 10},
+
+    {"id": "raspb",
+     "nameLabel": "Raspberry Pi",
+     "cost": 2,
+     "power": 20},
+
+    {"id": "arduino",
+     "nameLabel": "Arduino",
+     "cost": 3,
+     "power": 30},
+
+    {"id": "cpu",
+     "nameLabel": "CPU",
+     "cost": 4,
+     "power": 40},
+
+    {"id": "gpu",
+     "nameLabel": "GPU",
+     "cost": 5,
+     "power": 50},
+
+    {"id": "saugroboter",
+     "nameLabel": "Saugroboter",
+     "cost": 6,
+     "power": 60},
+
+    {"id": "mac",
+     "nameLabel": "Macintosh",
+     "cost": 7,
+     "power": 70},
+
+    {"id": "roboterarm",
+     "nameLabel": "Roboterarm",
+     "cost": 8,
+     "power": 80}
+)
