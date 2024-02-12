d9e94bb7114dad92219b7558d3d8f456a4f43a19
okh-mzny <60819623+okh-mzny@users.noreply.github.com>
2024-02-12 03:25:23 +0100

    turn into class

--- gameLogic/main/math.py
+++ gameLogic/main/math.py
@@ -1,110 +1,113 @@
 import time
 
-curr_score=0
+class MathLogic:
 
-Itemsdict={
-    "Taschenrechner":{
-        "Name": "Taschenrechner",
-        "Number": 13,
-        "Power": 10
-    },
-    "RaspberryPi":{
-        "Name": "RaspberryPi",
-        "Number": 5,
-        "Power": 20
+    curr_score=0
+
+    Itemsdict={
+        "Taschenrechner":{
+            "Name": "Taschenrechner",
+            "Number": 13,
+            "Power": 10
+        },
+        "RaspberryPi":{
+            "Name": "RaspberryPi",
+            "Number": 5,
+            "Power": 20
+        }
     }
-}
 
-def AddNumtoItem(Num,Itemname):
-    for Name in Itemsdict:
-        if(Name==Itemname):
-            Itemsdict[Name]["Number"]+=Num
+    def AddNumtoItem(self, Num,Itemname):
+        for Name in self.Itemsdict:
+            if(Name==Itemname):
+                self.Itemsdict[Name]["Number"]+=Num
 
-def AddPowtoItem(Pow,Itemname):
-    for Name in Itemsdict:
-        if(Name==Itemname):
-            Itemsdict[Name]["Power"]+=Pow
+    def AddPowtoItem(self, Pow,Itemname):
+        for Name in self.Itemsdict:
+            if(Name==Itemname):
+                self.Itemsdict[Name]["Power"]+=Pow
 
-def MultPowofItem(Mult,Itemname):
-    for Name in Itemsdict:
-        if(Name==Itemname):
-            Itemsdict[Name]["Power"]=Itemsdict[Name]["Power"]*Mult
+    def MultPowofItem(self, Mult,Itemname):
+        for Name in self.Itemsdict:
+            if(Name==Itemname):
+                self.Itemsdict[Name]["Power"]=self.Itemsdict[Name]["Power"]*Mult
 
-def get_score():
-    return curr_score
+    def get_score(self):
+        return curr_score
 
-def Add_toScore(Scoreadd):
-    global curr_score
-    curr_score+=Scoreadd
+    def Add_toScore(self, Scoreadd):
+        global curr_score
+        curr_score+=Scoreadd
 
-def increment():
-    total_incr=0
-    for Item in Itemsdict:
-        total_incr+=Itemsdict[Item]["Number"]*Itemsdict[Item]["Power"]
-    return total_incr
+    def increment(self):
+        total_incr=0
+        for Item in self.Itemsdict:
+            total_incr+=self.Itemsdict[Item]["Number"]*self.Itemsdict[Item]["Power"]
+        return total_incr
 
-def calc_twolog(rest_score):
-    if(rest_score>=2):
-        twolog=calc_twolog(rest_score/2)+1
+    def calc_twolog(self, rest_score):
+        if(rest_score>=2):
+            twolog=self.calc_twolog(rest_score/2)+1
+            return twolog
+        else:
+            return 0
+
+    def get_twolog(self):
         return twolog
-    else:
-        return 0
 
-def get_twolog():
-    return twolog
+    def calc_tenlog(self, rest_score):
+        if(rest_score>=10):
+            tenlog=self.calc_tenlog(rest_score/10)+1
+            return tenlog
+        else:
+            return 0
 
-def calc_tenlog(rest_score):
-    if(rest_score>=10):
-        tenlog=calc_tenlog(rest_score/10)+1
+    def get_tenlog(self):
         return tenlog
-    else:
-        return 0
 
-def get_tenlog():
-    return tenlog
+    def pot2(self, exp):
+        if(exp==0):
+            return 1
+        else:
+            pot=1
+            for i in range(exp):
+                pot=pot*2
+        return pot
 
-def pot2(exp):
-    if(exp==0):
-        return 1
-    else:
-        pot=1
-        for i in range(exp):
-            pot=pot*2
-    return pot
-
-def pot(exp):
-    if(exp==0):
-        return 1
-    else:
-        pot=1
-        for i in range(exp):
-            pot=pot*10
-    return pot
+    def pot(self, exp):
+        if(exp==0):
+            return 1
+        else:
+            pot=1
+            for i in range(exp):
+                pot=pot*10
+        return pot
 
 if(__name__=="__main__"):
+    mathlogic = MathLogic()
     while True:
-        twolog=calc_twolog(get_score())
+        twolog=mathlogic.calc_twolog(mathlogic.get_score())
         if(twolog<10):
-            new_score=curr_score
-            print("{:.2f}".format(curr_score)+"Byte")
+            new_score=mathlogic.curr_score
+            print("{:.2f}".format(mathlogic.curr_score)+"Byte")
         elif(twolog<20):
-            new_score=curr_score/pot2(10)
-            print("{:.2f}".format(curr_score/pot2(10))+"kB")
+            new_score=mathlogic.curr_score/mathlogic.pot2(10)
+            print("{:.2f}".format(mathlogic.curr_score/mathlogic.pot2(10))+"kB")
         elif(twolog<30):
-            new_score=curr_score/pot2(20)
-            print("{:.2f}".format(curr_score/pot2(20))+"MB")
+            new_score=mathlogic.curr_score/mathlogic.pot2(20)
+            print("{:.2f}".format(mathlogic.curr_score/mathlogic.pot2(20))+"MB")
         elif(twolog<40):
-            new_score=curr_score/pot2(30)
-            print("{:.2f}".format(curr_score/pot2(30))+"GB")
+            new_score=mathlogic.curr_score/mathlogic.pot2(30)
+            print("{:.2f}".format(mathlogic.curr_score/mathlogic.pot2(30))+"GB")
         elif(twolog<50):
-            new_score=curr_score/pot2(40)
-            print("{:.2f}".format(curr_score/pot2(40))+"TB")
+            new_score=mathlogic.curr_score/mathlogic.pot2(40)
+            print("{:.2f}".format(mathlogic.curr_score/mathlogic.pot2(40))+"TB")
         else:
-            new_score=curr_score/pot2(40)
-            tenlog=calc_tenlog(new_score)
-            print("{:.2f}".format(new_score/pot(tenlog))+"e"+str(tenlog)+" TB")
-        Add_toScore(increment())
-        AddNumtoItem(1,"Taschenrechner")
-        AddPowtoItem(1,"RaspberryPi")
-        MultPowofItem(1.01,"Taschenrechner")
+            new_score=mathlogic.curr_score/mathlogic.pot2(40)
+            tenlog=mathlogic.calc_tenlog(new_score)
+            print("{:.2f}".format(new_score/mathlogic.pot(tenlog))+"e"+str(tenlog)+" TB")
+        mathlogic.Add_toScore(mathlogic.increment())
+        mathlogic.AddNumtoItem(1,"Taschenrechner")
+        mathlogic.AddPowtoItem(1,"RaspberryPi")
+        mathlogic.MultPowofItem(1.01,"Taschenrechner")
         time.sleep(1)
