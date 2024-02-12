cb46c9864b2929e590e1ed7881f7bb8d9fd423ef
okh-mzny <60819623+okh-mzny@users.noreply.github.com>
2024-02-12 12:18:35 +0100

    Initialized itemDict in GUI

--- gui/guiMain.py
+++ gui/guiMain.py
@@ -4,7 +4,16 @@ from PyQt6 import QtWidgets, QtGui, uic
 
 import gameLogic.main.items as itemDefinition
 
-cols = [None, 'nameLabel', 'oneGeneratingLabel', 'costLabel', 'buyButton', 'totalOwnedLabel', 'totalGeneratingLabel']
+import gameLogic.main.math as gameMath
+import gameLogic.shop.buyfuctions as gameBuyFunctions
+
+cols = ['nameLabel',
+        'oneGeneratingLabel',
+        'costLabel',
+        'buyButton',
+        'totalOwnedLabel',
+        'totalGeneratingLabel'
+]
 
 
 def init():
@@ -15,10 +24,17 @@ def init():
 
 
 class MainWindow(QtWidgets.QMainWindow):
+    gameMathObj = None
+    gameShopObj = None
+
     shopItemWidgets = {}
 
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.gameMathObj = gameMath.MathLogic()
+        self.gameShopObj = gameBuyFunctions.BuyFunctions(self.gameMathObj)
+
         uic.loadUi('gui/uic/gui.ui', self)
         self.pushButton.setIcon(QtGui.QIcon('gui/res/pushBtn.png'))
 
@@ -28,7 +44,7 @@ class MainWindow(QtWidgets.QMainWindow):
                 'nameLabel': QtWidgets.QLabel(),
                 'oneGeneratingLabel': QtWidgets.QLabel(),
                 'costLabel': QtWidgets.QLabel(),
-                'buyButton': QtWidgets.QPushButton(),
+                'buyButton': QtWidgets.QPushButton("Buy"),
                 'totalOwnedLabel': QtWidgets.QLabel(),
                 'totalGeneratingLabel': QtWidgets.QLabel()
             }
@@ -36,3 +52,15 @@ class MainWindow(QtWidgets.QMainWindow):
             for col, col_name in enumerate(cols):
                 if col_name is not None:
                     self.shopTable.setCellWidget(newRowCount, col, self.shopItemWidgets[item['id']][col_name])
+            self.shopTable.cellWidget(newRowCount, cols.index('nameLabel')).setText(item['nameLabel'])
+            self.shopTable.cellWidget(newRowCount, cols.index('oneGeneratingLabel')).setText(f'{item['power']}')
+            self.shopTable.cellWidget(newRowCount, cols.index('costLabel')).setText(f'{item['cost']}')
+            self.shopTable.cellWidget(newRowCount, cols.index('buyButton')).clicked.connect(self.clickHandler)
+
+    def clickHandler(self):
+        item_id = ""
+        for item in itemDefinition.items:
+            if self.sender() is self.shopItemWidgets[item['id']]['buyButton']:
+                item_id = item['id']
+                break
+        print(item_id)
